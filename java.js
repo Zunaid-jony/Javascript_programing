e0d2510a135396b39756b1c3268159d14445abaa
Zunaid-jony <jony35666666@gmail.com>
2022-04-01 23:53:26 +0600

    update

---
+++ java.js
@@ -0,0 +1,8 @@
+function makRed(){
+    document.body.style.backgroundColor='red';
+    
+}
+const soboj = document.getElementById('click4');
+soboj.onclick = function(){
+    document.body.style.backgroundColor='DarkGreen';
+}
