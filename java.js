e11c287c70953468695b4493f13611a9e434279c
Zunaid-jony <jony35666666@gmail.com>
2022-04-04 08:07:38 +0600

    last update

--- java.js
+++ java.js
@@ -6,3 +6,7 @@ const soboj = document.getElementById('click4');
 soboj.onclick = function(){
     document.body.style.backgroundColor='DarkGreen'; 
 }
+const updateColor = document.getElementById('update-color');
+updateColor.onclick = function(){
+    document.body.style.backgroundColor='gray';
+}
